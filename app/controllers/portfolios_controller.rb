3ebaed61c3efd3ddcd50be4e05921a4a2339a6de
disrup <disruptivenewmind@gmail.com>
2018-10-25 17:07:03 -0500

    controller edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,9 +11,25 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new(portfolio_params)
 		respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
       else
         format.html { render :new }
+      end
+    end
+	end
+
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end 
+
+	def update
+		@portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to @portfolio_item, notice: 'Portfolio Item was successfully updated.' }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
 	end
