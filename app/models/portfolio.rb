396b1f099a6f3f8112f29fae0d20c5fabd0bbabb
disrup <disruptivenewmind@gmail.com>
2018-11-08 18:55:33 -0600

    concerns for data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	puts "SELF DENTRO DE Portfolio Class es igual a #{self}  " * 3
 	validates :title, :body, :main_image, :thumb_image, presence: true
 
@@ -12,8 +13,8 @@ class Portfolio < ApplicationRecord
 
   def set_defaults
   	puts "SELF DENTRO DE set_defaults es igual a #{self}" * 3
-  	self.main_image ||= "http://placehold.it/600x400"
-		self.thumb_image ||= "http://placehold.it/350x200"
+  	self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
+		self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
   end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,12 @@
 class Skill < ApplicationRecord
+	include Placeholder
 	validates :title, :percent_utilized, presence: true
+
+	after_initialize :set_defaults
+
+  def set_defaults
+  	puts "SELF DENTRO DE set_defaults es igual a #{self}" * 3
+  	self.badge ||= Placeholder.image_generator(height: "250", width: "250")
+  end
+
 end
