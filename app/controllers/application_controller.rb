c7edc341088b5d5ef6d5b15299686c09245dd11e
disrup <disruptivenewmind@gmail.com>
2018-11-25 15:49:55 -0600

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :set_copyright
 
   include DeviseWhitelistConcern
   include SetSourceConcern
   include	CurrentUserConcern
   include SetTitleConcern
 
-end 
+  def set_copyright
+		@copyright = JavierOrtizTool::Renderer.copyright("Javier Ortiz Arellano", "Programado en Ruby on Rails" )
+	end
+
+end
+
+module JavierOrtizTool
+	class Renderer
+		def self.copyright(name, msg)
+			"<b>#{name}</b> | &copy; #{Time.now.year} <br> #{msg} ".html_safe
+		end
+	end	
+end
