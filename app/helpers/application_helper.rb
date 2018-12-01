883530422a07c84b81231afafb0571f8b58420f4
disrup <disruptivenewmind@gmail.com>
2018-12-01 14:26:52 -0600

    Styles for home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-	def login_helper
+	def login_helper(style)
 		if current_user.is_a?(GuestUser)
-		  (link_to "Register", new_user_registration_path) + #para que el método regrese dos cosas concatenamos los links_to de register y login
-		  "<br>".html_safe + 
-		  (link_to "Login", new_user_session_path)
+		  (link_to "Register", new_user_registration_path, class: style) + " ".html_safe + #para que el método regrese dos cosas concatenamos los links_to de register y login
+		  
+		  (link_to "Login", new_user_session_path, class: style)
 		else
-		  link_to "Logout", destroy_user_session_path, method: :delete
+		  link_to("Logout", destroy_user_session_path, method: :delete, class: style)
 		end
 	end
 
