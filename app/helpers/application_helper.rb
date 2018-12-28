1ebe99bcc4be3f64331ce0f57a18159762d48da3
disrup <disruptivenewmind@gmail.com>
2018-12-27 21:27:39 -0600

    Portfolio cambios vistas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 
 	def login_helper(style = "")
 		if current_user.is_a?(GuestUser)
-		  (link_to "Register", new_user_registration_path, class: style) + " ".html_safe + #para que el método regrese dos cosas concatenamos los links_to de register y login
-		  
+			#IMPORTANTE!!! para que el método regrese dos cosas concatenamos los links_to de register y login
+		  (link_to "Register", new_user_registration_path, class: style) + " ".html_safe + 
 		  (link_to "Login", new_user_session_path, class: style)
 		else
 		  link_to("Logout", destroy_user_session_path, method: :delete, class: style)
